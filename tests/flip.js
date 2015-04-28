fa7016887d6aa5a6517846916b28ec4723ed80a0
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-28 16:44:43 +0200

    Add new function `flip`
    ### flip
    
    Flip a function’s arguments.
    
    ```js
    var flip = require(1-liners/flip);
    
    var f = (a, b) => a / b;
    
    flip(f)(2, 6);        // => 3
    flip(flip(f))(6, 2);  // => 3
    ```

---
+++ tests/flip.js
@@ -0,0 +1,12 @@
+import { equal } from 'assert';
+import flip from '../flip';
+
+test('#flip', () => {
+  const f = (a, b) => a / b;
+  equal(flip(f)(2, 6), 3);
+  equal(flip(flip(f))(6, 2), 3);
+
+  const g = (a, b, c) => a / (b + c);
+  equal(flip(g)(1, 2, 3), 1);
+  equal(flip(flip(g))(3, 2, 1), 1);
+});
