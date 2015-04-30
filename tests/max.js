296607702371b44f9246d83de6a718acdb011872
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-30 23:29:27 +0200

    Improve test coverage

--- tests/max.js
+++ tests/max.js
@@ -3,5 +3,7 @@ import max from '../max';
 
 test('#max', () => {
 	equal(max(3, 6), 6);
+	equal(max(6, 3), 6);
+
 	equal([3, 6, 9].reduce(max), 9);
 });
--- tests/min.js
+++ tests/min.js
@@ -3,5 +3,7 @@ import min from '../min';
 
 test('#min', () => {
 	equal(min(3, 6), 3);
+	equal(min(6, 3), 3);
+
 	equal([3, 6, 1].reduce(min), 1);
 });
