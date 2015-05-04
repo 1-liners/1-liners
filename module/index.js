2280702c3940e2fc221b78f101baf2f9051adfd8
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-04 19:06:46 +0200

    Get rid of curry1

--- module/index.js
+++ module/index.js
@@ -2,7 +2,6 @@ import and from './and';
 import bitAnd from './bitAnd';
 import bitOr from './bitOr';
 import compose from './compose';
-import curry1 from './curry1';
 import curry2 from './curry2';
 import curry3 from './curry3';
 import curry4 from './curry4';
@@ -54,7 +53,6 @@ export default {
   bitAnd,
   bitOr,
   compose,
-  curry1,
   curry2,
   curry3,
   curry4,
