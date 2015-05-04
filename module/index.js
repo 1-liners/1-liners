5a7480381bb65a69f0a79aef12a0f7f8e73cdddb
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-04 19:06:07 +0200

    Update the index
    Sorry, I forgot!

--- module/index.js
+++ module/index.js
@@ -6,6 +6,7 @@ import curry1 from './curry1';
 import curry2 from './curry2';
 import curry3 from './curry3';
 import curry4 from './curry4';
+import curryRight2 from './curryRight2';
 import dec from './dec';
 import equal from './equal';
 import every from './every';
@@ -58,6 +59,7 @@ export default {
   curry2,
   curry3,
   curry4,
+  curryRight2,
   dec,
   equal,
   every,
