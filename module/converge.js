b6eb7ec1617541438ed30523f9485e028900e6ae
Christoph Hermann <christoph.hermann@kaba.com>
2015-05-06 15:40:34 +0200

    adds jsdoc to converge
    - closes #58

--- module/converge.js
+++ module/converge.js
@@ -1,1 +1,16 @@
+/**
+ * @module 1-liners/converge
+ *
+ * @description
+ *
+ * Converge two functions into one.
+ *
+ * @example
+ *
+ *   var converge = require('1-liners/converge');
+ *
+ *   converge(f, g, h)(1, 2) === f(g(1, 2), h(1, 2));
+ *
+ *
+ */
 export default (f, g, h) => (...args) => f(g(...args), h(...args));
