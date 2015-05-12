d1e8e38a8c8d9313543d1bf18bd996867cf505dd
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-12 12:24:07 +0200

    Document usage of `plus` with strings

--- module/plus.js
+++ module/plus.js
@@ -1,15 +1,16 @@
 /**
  * @module 1-liners/plus
- * 
+ *
  * @description
  *
  * Same as `a + b`.
- * 
+ *
  * @example
- * 
+ *
  * 	var plus = require('1-liners/plus');
- * 
- * 	plus(2, 8); // => 10
- * 
+ *
+ * 	plus(2, 8);      // => 10
+ * 	plus('a', 'b');  // => 'ab'
+ *
  */
 export default (a, b) => a + b;
