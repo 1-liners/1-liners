187a802b0dade84dd79c8b1a2e98012efdec77eb
K Shirish <kshirish@users.noreply.github.com>
2015-08-26 11:19:38 +0530

    Update isNaN.js

--- module/isNaN.js
+++ module/isNaN.js
@@ -4,7 +4,7 @@
  * @description
  *
  * Check if the number is NaN
- * 
+ * Same as https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/isNaN
  * @example
  * 
  * 	var isNaN = require('1-liners/isNaN');
