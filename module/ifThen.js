df15fd8a1dcb6cb25acf0d19a20668533b2e57fe
Dmitri Kunin <whitemarten@gmail.com>
2016-04-23 23:29:35 +0300

    Removed unnecessary from doc-block of ifThen, added then example.

--- module/ifThen.js
+++ module/ifThen.js
@@ -10,13 +10,10 @@
  *
  * 	const ifThen = require('1-liners/ifThen');
  *
- * 	const eq = (a, b) => a === b;
- * 	const add = (a, b) => a + b;
- * 	const sub = (a, b) => a - b;
- *
  * 	const words = ifThen((str) => typeof str === 'string', (str) => str.split(' '));
  *
  * 	words('Hello ES2015'); // => ['Hello', 'ES2015']
+ *     words(['Hello', 'ES2015']); // => undefined
  *
  */
 export default (perdicate, then) => (...args) => perdicate(...args) ? then(...args) : undefined;
