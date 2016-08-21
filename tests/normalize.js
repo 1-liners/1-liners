2c5ef7a8737e66ef53d3b983f1dfde2699e3ac8e
srinivas <srinivasdamam@gmail.com>
2016-08-21 17:44:26 +0530

    Fixed normalize function throwing range error in the test cases.

--- tests/normalize.js
+++ tests/normalize.js
@@ -1,9 +1,11 @@
-import { equal } from 'assert';
+import { equal, throws } from 'assert';
 import normalize from '../normalize';
 
 test('#normalize', () => {
   let str = '\u1E9B\u0323';
-  equal(normalize(null, str), 'ẛ̣');
+  throws(() => {
+    normalize(null, str), RangeError, 'The normalization form should be one of NFC, NFD, NFKC, NFKD.'
+  });
   equal(normalize('NFC', str), 'ẛ̣');
   equal(normalize('NFD', str), 'ẛ̣');
   equal(normalize('NFKC', str), 'ṩ');
